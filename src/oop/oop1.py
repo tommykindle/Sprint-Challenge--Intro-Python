d4c55ff7312b51c20dff40e0ad3809db6d6b3284
Tommy <tommykindle83@gmail.com>
2020-07-19 18:54:45 -0700

    working on classes have a bug with GroundVechicle

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -24,3 +24,19 @@ class Vehicle:
 
 class FlightVehicle(Vehicle):
     pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
